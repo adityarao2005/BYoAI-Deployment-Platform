838cbb400bf5f4d40408eb471f7adbe772d389d1
Aditya Rao <adityarao2005@gmail.com>
2026-06-07 13:46:35 -0400

    removed openliberty stuff unforunately

--- agentic-harness/src/main/java/com/github/adityarao2005/harness/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package com.github.adityarao2005.harness;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class RestApplication extends Application {
-
-}
