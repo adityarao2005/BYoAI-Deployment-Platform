ad4b8e67152e7980bc1b3287004243ad0a019bf4
Aditya Rao <adityarao2005@gmail.com>
2026-08-01 14:41:31 -0400

    created the tool config for the OpenAPI tool provider

---
+++ agentic_harness/src/config/tool_config.ts
@@ -0,0 +1,49 @@
+import z from "zod";
+
+export const BaseToolProviderConfigSchema = z.object({
+    name: z.string()
+})
+
+export const OpenAPIToolProviderConfigSchema = BaseToolProviderConfigSchema.extend({
+    type: z.literal("openapi"),
+    specUrl: z.string(),
+    securityVariables: z.discriminatedUnion("type", [
+        z.object({
+            type: z.literal("apiKey"),
+            key: z.string(),
+            name: z.string().default("X-API-Key"),
+            location: z.enum(["header", "query", "cookie"]).default("header")
+        }),
+        z.object({
+            type: z.literal("bearerToken"),
+            token: z.string()
+        }),
+        z.object({
+            type: z.literal("basicAuth"),
+            username: z.string(),
+            password: z.string(),
+            location: z.enum(["header", "authority"]).default("header")
+        }),
+        z.object({
+            type: z.literal("custom"),
+            headers: z.record(z.string(), z.string()).default({}),
+            queryParams: z.record(z.string(), z.string()).default({}),
+            pathParams: z.record(z.string(), z.string()).default({}),
+            urlAuthority: z.object({
+                user: z.string().optional(),
+                password: z.string().optional()
+            }).optional(),
+        }),
+
+        // TODO: implement OAuth2 support in the future when we make this an actual server-side component that can handle the OAuth2 flow. For now, we just define the schema for it.
+        z.object({
+            type: z.literal("oauth2")
+        })
+    ])
+})
+
+export const ToolProviderConfigSchema = z.discriminatedUnion("type", [
+    OpenAPIToolProviderConfigSchema
+])
+
+export type ToolProviderConfig = z.infer<typeof ToolProviderConfigSchema>;
